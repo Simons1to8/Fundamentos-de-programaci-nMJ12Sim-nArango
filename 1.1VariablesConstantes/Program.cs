5d6fc9211dfa7c45508273f3c748b0f5a645cc52
Simons1to8 <simon.arangov@upb.edu.co>
2026-07-28 13:35:43 -0500

    mi primer commit

---
+++ 1.1VariablesConstantes/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace _1._1VariablesConstantes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Aqui escribo el codigo fuente 
+            string nombre = "Simón Arango";
+            Console.WriteLine("Bienvenido" + " " + nombre + " " + "al curso de fundamentos de programacion");
+
+        }
+    }
+}
